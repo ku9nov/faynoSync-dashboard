1a64950e0b92b52088cb87bf0c3e7e79ce40946b
ku9nov <sergeyku9nov@gmail.com>
2025-04-10 16:16:31 +0300

    add themes to signup signin

--- src/components/inputs/authInputs.tsx
+++ src/components/inputs/authInputs.tsx
@@ -18,7 +18,7 @@ export const AuthInputs: FC<AuthInputsProps> = ({
         name={name}
         type={type}
         placeholder={placeholder}
-        className='w-full px-4 py-3 border border-purple-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 font-sans'
+        className='w-full px-4 py-3 border border-theme-auth-input rounded-md focus:outline-none focus:ring-2 focus:ring-theme-auth-input font-sans'
       />
       <ErrorMessage
         name={name}
